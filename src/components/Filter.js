dc860655921915ddb0d6f56eddba588e117fa3fc
sfcc5555 <sfcc.5555@gmail.com>
2023-04-19 15:55:47 -0500

    a

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { FilterMenu } from "./FilterMenu";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 const Filter = ({mode})=> {
 
