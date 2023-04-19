15a0386004f5e7237cba85ae6202ef9f72fc1c0f
sfcc5555 <sfcc.5555@gmail.com>
2023-04-18 23:10:42 -0500

    a

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,6 +1,6 @@
 const Filter = ({mode})=> {
     return (
-        <button>Filter by Region</button>
+        <button className={`relative text-left pl-5 filterButton h-12 rounded-md ${mode}Shadow bg-${mode}ModeElements text-${mode}ModeText`} style={{width:200}}>Filter by Region<span className="absolute right-5 top-5 w-2 h-2 inline-block bg-no-repeat bg-center bg-contain" style={{ backgroundImage: `url(${require(`../assets/icons/${mode}DownArrowIcon.png`)})` }}/></button>
     )
 }
 
