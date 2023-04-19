0db843406e3f5cd1d674f21f53fd558b919b67f4
sfcc5555 <sfcc.5555@gmail.com>
2023-04-19 15:41:27 -0500

    a

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,7 +1,42 @@
+import React from "react";
+import { FilterMenu } from "./FilterMenu";
+import { useState, useEffect } from "react";
+
 const Filter = ({mode})=> {
+
+    const [filterMenu,setFilterMenu] = useState(false);
+
+    function controlFilterMenu() {
+
+            filterMenu?setFilterMenu(false):setFilterMenu(true);
+    }
+
+    function closeFilterMenu(e) {
+        
+        if (e.target.id!=='filterButton') {
+            setFilterMenu(false);
+        }
+    }
+
+
     return (
-        <button className={`relative text-left pl-5 filterButton h-12 rounded-md ${mode}Shadow bg-${mode}ModeElements text-${mode}ModeText`} style={{width:200}}>Filter by Region<span className="absolute right-5 top-5 w-2 h-2 inline-block bg-no-repeat bg-center bg-contain" style={{ backgroundImage: `url(${require(`../assets/icons/${mode}DownArrowIcon.png`)})` }}/></button>
+
+        <div className="relative text-sm font-semibold">
+            <button id='filterButton' onClick={controlFilterMenu} className={`relative text-left pl-5 filterButton h-12 rounded-md ${mode}Shadow bg-${mode}ModeElements text-${mode}ModeText hover:opacity-70`} style={{width:175}}>
+            
+                Filter by Region
+                
+                <span className="absolute right-5 top-5 w-2 h-2 inline-block bg-no-repeat bg-center bg-contain" style={{ backgroundImage: `url(${require(`../assets/icons/${mode}DownArrowIcon.png`)})` }}/>
+
+            </button>
+
+            {filterMenu&&<FilterMenu mode = {mode} closeFilterMenu={closeFilterMenu} />}
+
+        </div>
+
+
     )
 }
 
 export { Filter };
+
---
+++ src/components/FilterMenu.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useEffect } from "react";
+
+const FilterMenu = ({ mode, closeFilterMenu}) => {
+
+
+    useEffect(()=> {
+        
+        document.addEventListener('click',closeFilterMenu);
+
+        return ()=> {
+
+            document.removeEventListener('click',closeFilterMenu);
+        }
+
+
+    },[])
+
+
+    return (
+    <section className={`absolute flex flex-col gap-2 p-4 left-0 rounded-md ${mode}Shadow bg-${mode}ModeElements text-${mode}ModeText`} style={{width:175, top:54}}> 
+
+        <div className="cursor-pointer hover:opacity-70">Africa</div>
+        <div className="cursor-pointer hover:opacity-70">America</div>
+        <div className="cursor-pointer hover:opacity-70">Asia</div>
+        <div className="cursor-pointer hover:opacity-70">Europe</div>
+        <div className="cursor-pointer hover:opacity-70">Oceania</div>
+        <div className="cursor-pointer hover:opacity-70">All</div>
+
+    </section>
+    )
+}
+
+export { FilterMenu };
