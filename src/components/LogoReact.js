1d977b3b72f142544454a8398529cf5f4f8381d1
sfcc5555 <sfcc.5555@gmail.com>
2023-04-19 15:50:42 -0500

    a

--- src/components/LogoReact.js
+++ src/components/LogoReact.js
@@ -4,7 +4,7 @@ import '../styles/LogoReact.css'
 const LogoReact = () => {
     return (
     <div className="fixed text-center text-gray-500 flex items-center bottom-8 right-0">
-        <p className="text-base" >Powered by React.</p>
+        <p className="text-sm" >Powered by React.</p>
         <img src={logo} className="reactLogo cursor-crosshair" alt="logo" />
     </div>)
 }
